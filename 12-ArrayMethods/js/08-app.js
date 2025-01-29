ef8c1f07ea4f962d767cee9b31e2f13fbd03047b
Harold de las Heras <haroldherascabezas@hotmail.com>
2025-01-29 14:24:48 +0100

    Acabada la carpeta 12

--- 12-ArrayMethods/js/08-app.js
+++ 12-ArrayMethods/js/08-app.js
@@ -1,5 +1,7 @@
 
 
+/*EJEMPLOS Y USOS DE EL "SPREAD OPERATOR*/
+
 
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
@@ -11,3 +13,23 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ];
+
+
+let meses2 = [...meses, "Agosto"];                  //Clonamos el primer array añadiendo otro elemento al final
+meses2 = ["añadimos al inicio",...meses2];           //Añadimos al segundo array otro elemento al principio
+meses2 = [...meses2, ..."Hola"];                    //Añadimos un String con los puntos, y lo añade separado por letras
+console.log(meses2);
+
+
+console.log("-------------------------");
+
+
+//Spread operator con Array de objetos
+
+const producto = {nombre: "Disco Duro", precio: 300};
+
+const carrito2 = [...carrito, producto];
+
+console.log(carrito2);
+
+
