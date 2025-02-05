ecfa235535e4feddd9aaa5fe58d057bd3e4e671a
Harold de las Heras <haroldherascabezas@hotmail.com>
2025-02-05 12:35:46 +0100

    13-DOM con comentarios

--- 13-DOM/js/11-scripts.js
+++ 13-DOM/js/11-scripts.js
@@ -1,21 +1,28 @@
 
 
-
-const btnFlotante = document.querySelector(".btn-flotante");
-
-const footer = document.querySelector(".footer");
-
-
-btnFlotante.addEventListener("click", mostrarOcultarFooter);
+/**Modificamos el CSS de la página web desde JS. Le damos funcionalidad al botón del footer de la página para que expanda 
+ * o repliegue el menú emergente.
+ */
 
 
+const btnFlotante = document.querySelector(".btn-flotante");            //Almacenamos en una variable el botón
 
-function mostrarOcultarFooter () {
-    if(footer.classList.contains("activo")){
+const footer = document.querySelector(".footer");                       //Almacenamos en una variable el footer
+
+
+btnFlotante.addEventListener("click", mostrarOcultarFooter);            /*Añadimos un EventListener al botón. Le pasamso por parámetro
+                                                                        cuando se va a ctivar, y que va a hacer al activarse*/
+
+
+
+
+/*Creamos al función que responderá al evento del botón. Cambiando la clase del footer y del botón, haremos que haga una cosa u otra*/
+function mostrarOcultarFooter () {                                      
+    if(footer.classList.contains("activo")){                //Si el footer contiene la clase "activo" entonces se le quita al footer y al botón
         footer.classList.remove("activo");
         btnFlotante.classList.remove("activo");
-        btnFlotante.textContent = "Iidoma y Moneda";
-    }else{
+        btnFlotante.textContent = "Iidoma y Moneda";        
+    }else{                                                  //Si no al contiene, se le añade
         footer.classList.add("activo");
         btnFlotante.classList.add("activo");
         btnFlotante.textContent = "X Cerrar";
